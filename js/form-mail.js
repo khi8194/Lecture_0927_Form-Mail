f1643cb889ced02f8eee9f5d1f00e36f60601d66
khi8194 <khi8194@gmail.com>
2024-09-27 13:30:28 +0900

    미입력 항목 존재시 메일 전송 중지

--- js/form-mail.js
+++ js/form-mail.js
@@ -22,9 +22,13 @@ const form = document.querySelector("#contact-form");
 const user_name = form.querySelector("[name=user_name]");
 const user_email = form.querySelector("[name=user_email]");
 const message = form.querySelector("textarea");
+
 form.addEventListener("submit", event => {
 	event.preventDefault();
-	//sendForm(serviceID,  templateID, formEl)
+
+	//3개 중에 하나라도 입력항목이 비어있으면 경고장 출력하면서 메인 전송 중지
+	if (!user_name.value.trim() || !user_email.value.trim() || !message.value.trim()) return alert("모든 입력 항목을 작성해주세요!");
+
 	emailjs.sendForm("service_cl2vkl5", "template_la0d34r", form).then(
 		() => {
 			user_name.value = "";
